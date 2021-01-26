43d9ab3c9f8c18ac595fd371d6c1ad04c5acc7d8
Vick <vickram.agarwal@gmail.com>
2021-01-26 03:16:06 +0000

    updated with ruby

--- app/actions.rb
+++ app/actions.rb
@@ -6,8 +6,9 @@ def humanized_time_ago(time_ago_in_minutes)
   end
 end
 
+
 get '/' do
-  finstagram_post_shark = {
+  @finstagram_post_shark = {
     username: "sharky_j",
     avatar_url: "http://naserca.com/images/sharky_j.jpg",
     photo_url: "http://naserca.com/images/shark.jpg",
@@ -20,7 +21,7 @@ get '/' do
     }]
   }
 
-  finstagram_post_whale = {
+  @finstagram_post_whale = {
     username: "kirk_whalum",
     avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
     photo_url: "http://naserca.com/images/whale.jpg",
@@ -33,7 +34,7 @@ get '/' do
     }]
   }
 
-  finstagram_post_marlin = {
+  @finstagram_post_marlin = {
     username: "marlin_peppa",
     avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
     photo_url: "http://naserca.com/images/marlin.jpg",
@@ -45,5 +46,8 @@ get '/' do
       text: "lunchtime! ;)"
     }]
   }
-  [finstagram_post_shark, finstagram_post_whale, finstagram_post_marlin].to_s
+
+   @finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
+
+  erb(:index)
 end
