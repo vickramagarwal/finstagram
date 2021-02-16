78c81bca3f2acf65461b5b18d30f0b13318aa8e0
Vick <vickram.agarwal@gmail.com>
2021-02-16 02:21:39 +0000

    changes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,3 +4,14 @@ class Comment < ActiveRecord::Base
   belongs_to :finstagram_post
 
 end
+
+class Comment < ActiveRecord::Base
+
+  # Associations from a previous exercise
+  belongs_to :user
+  belongs_to :finstagram_post
+
+  # New validation code
+  validates_presence_of :text, :user, :finstagram_post
+
+end
